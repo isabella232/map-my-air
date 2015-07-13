c667e9a415bed5bfa41ec9f5b1a526d53c1966a4
Chris Marx <chrismarx@gmail.com>
2015-07-13 16:09:40 -0400

    updated defautl controlleras alias to match controller name and set reloadOnSearch to false, so updated to routeId search parameter from angular dont reload the page

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -2,10 +2,13 @@
 
 	angular.module('columbiaWync')
   .config(function ($routeProvider) {
+  	//TODO we could upgrade to angular ui router for more specific param mappings
     $routeProvider
       .when('/', {
         templateUrl: 'views/home/home.html',
         controller: 'HomeCtrl',
-        controllerAs: 'vm'
+        controllerAs: 'hc',
+        //we use the search property to determine what geojson layer is displayed, but we wont need to reload the map
+        reloadOnSearch: false
       });
   });
