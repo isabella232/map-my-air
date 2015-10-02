cebf3607286a0a7b1daf04c9b8578700225bbf58
Brian Whitton <me@brianwhitton.com>
2015-10-02 13:51:05 -0400

    moves backend url and namespace values to config file

--- ember/app/bike-route/adapter.js
+++ ember/app/bike-route/adapter.js
@@ -1,10 +1,17 @@
 import DS from 'ember-data';
+import config from 'polluted-routes/config/environment';
+
+const {
+  dbHost,
+  dbNamespace
+} = config;
 
 export default DS.RESTAdapter.extend({
-  host: 'http://columbia-wnyc.cartodb.com',
-  namespace: 'api/v2/sql',
-  query: '?format=GeoJSON&q=SELECT * FROM gpxline_part where gpxline_id = ',
+  host: dbHost,
+  namespace: dbNamespace,
+  query: 'format=GeoJSON&q=SELECT * FROM gpxline_part where gpxline_id',
   urlForFindRecord(id) {
-    return [this.host, this.namespace, `${this.query}${id}`].join('/')
+    const query = `${this.query}=${id}`
+    return `${dbHost}/${dbNamespace}?${query}`
   }
 })
--- ember/app/mixins/upload-mixin.js
+++ ember/app/mixins/upload-mixin.js
@@ -1,8 +1,14 @@
 import Ember from 'ember';
 import EmberUploader from 'ember-uploader';
+import config from 'polluted-routes/config/environment';
+
+const {
+  dbHost,
+  dbNamespace
+} = config;
 
 export default Ember.Mixin.create({
   uploader: EmberUploader.Uploader.create({
-    url: 'http://dev.wnyc.net:4570/gpxroute'
+    url: `${dbHost}/${dbNamespace}/gpxroute`
   })
 })
