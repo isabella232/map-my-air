0a316478e36ae2a0d5d304cbca6cb8c24d8b5108
Brian Whitton <me@brianwhitton.com>
2015-11-06 14:56:14 -0500

    updates upload-controls label based on route id

--- ember/app/components/upload-controls/component.js
+++ ember/app/components/upload-controls/component.js
@@ -5,12 +5,24 @@ const {
   $,
   set,
   get,
-  computed
+  computed,
+  observer
 } = Ember;
 
 export default Component.extend({
   classNames: ['upload-controls'],
   uploadDisabled: computed.bool('progress'),
+  dynamicLabel: 'Upload your biking data',
+
+  updateLabel: observer('routeId', function() {
+    const hasFile = get(this, 'hasFile')
+    const $fileInput = get(this, 'fileInput')
+    if (hasFile) {
+      set(this, 'dynamicLabel', 'Upload another route')
+      set(this, 'hasFile', false)
+      window.debugInput = $fileInput
+    }
+  }),
 
   didInsertElement() {
     set(this, 'fileInput', $('#fileInput'))
@@ -19,6 +31,9 @@ export default Component.extend({
   change() {
     const $fileInput = get(this, 'fileInput')
     const file = $fileInput.get(0).files[0]
+    if (!file) {
+      return
+    }
     const fileName = get(file, 'name')
 
     set(this, 'hasFile', true)
