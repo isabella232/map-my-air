7b1fddcb76d9cf66fdc96b5752ec30b55c680e0e
Brian Whitton <me@brianwhitton.com>
2015-11-06 14:48:09 -0500

    imports moment

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -24,6 +24,7 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
 
   app.import('bower_components/leaflet-plugins/layer/tile/Google.js')
+  app.import('bower_components/moment/moment.js')
 
   return app.toTree();
 };
