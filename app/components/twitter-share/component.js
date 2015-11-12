cdecb20aa87e3d93053ddd5c329fdd9887e04cc9
Brian Whitton <me@brianwhitton.com>
2015-11-11 21:16:42 -0500

    deployment updates
    fixes paths for local serving
    meta tags
    credit

--- app/components/twitter-share/component.js
+++ app/components/twitter-share/component.js
@@ -8,8 +8,8 @@ const {
 export default Component.extend({
   click() {
     const url = get(this, 'url') || window.location.toString();
-    const text = get(this, 'text') || 'Sharing my route';
-    const twitterUrl = `https://twitter.com/intent/tweet?url=${url}&text=${text}`;
+    const text = get(this, 'text') || 'Check out air pollution levels on my route. Try @WNYC @Columbia Map My Air.';
+    const twitterUrl = `https:\/\/twitter.com/intent/tweet?url=${encodeURIComponent(url)}&text=${text}`;
 
     window.open(twitterUrl, 'pop', 'width=600, height=400, scrollbars=no');
   }
