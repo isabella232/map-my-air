145ff4adb4311c70cc05383ef7581ae045f8ced3
Brian Whitton <me@brianwhitton.com>
2017-04-12 14:28:05 -0400

    config and deps

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-    storeConfigInMeta: false,
     fingerprint: {
         enabled: false
     }
