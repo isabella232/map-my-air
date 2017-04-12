3f22522ebe60690fd45671e652b6070c5a990bdc
Brian Whitton <me@brianwhitton.com>
2017-04-12 14:35:22 -0400

    turn on fingerprint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,10 +3,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
-    fingerprint: {
-        enabled: false
-    }
   });
 
   // Use `app.import` to add additional libraries to the generated
